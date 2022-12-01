fc22dd70c12fd0b8e32c83a0f56482549ddb25dd
Ben Kunkle <ben.kunkle@gmail.com>
2022-11-30 20:40:34 -0800

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --out-dir ./bin/ --no-typescript --browser --target web ./target/wasm32-unknown-unknown/release/ant-sim.wasm
+wasm-bindgen --out-dir ./bin/ --no-typescript --target web ./target/wasm32-unknown-unknown/release/ant-sim.wasm
